eb2378286b151bfe3815a9e8bd0c55bd4d3aa273
Ahmed Saber <smartic@gmail.com>
2022-10-26 16:04:05 +0200

    Fix the production build with the watchers

--- src/index.js
+++ src/index.js
@@ -21,45 +21,64 @@ module.exports = function rollupPluginIconfont(options = {}) {
 			throw new Error(`Require '${r}' option`);
 		}
 	}
+
 	options = Object.assign({}, options);
-	const build = (callback) => {
+
+	const build = callback => {
 		return nodify(
-			generate.byGlobby(options).then(result => {
-				return writeFiles(result);
-			}).then(ret => {
-				console.log('iconfont + css have been built with ' + ret.glyphDatas.length + ' svg-icons.');
-				options.success && options.success();
-				return ret;
-			}).catch(console.error.bind(console)),
+			generate
+				.byGlobby(options)
+				.then(result => {
+					return writeFiles(result);
+				})
+				.then(ret => {
+					console.log('iconfont + css have been built with ' + ret.glyphDatas.length + ' svg-icons.');
+					options.success && options.success();
+					return ret;
+				})
+				.catch(console.error.bind(console)),
 			error => callback && callback(error)
 		);
 	};
+
 	const watch = () => {
-		const comileDebounce = thro_debs.debounce(800, build.bind(this));
-		const svgs = [].concat(options.svgs);
+		if (process.env.NODE_ENV !== 'production') {
+			let watchers1;
+			let watchers2;
 
-		// 只有一个文件夹时，监视文件夹。支持新增。/ab/c/*.svg
-		if (svgs.length === 1) {
-			const dir = path.dirname(svgs[0]).replace('*.svg', '');
-			if (fs.existsSync(dir) && fs.statSync(dir).isDirectory()) {
-				fs.watch(dir, (event, filename) => {
-					if (filename.length > 4 && filename.slice(-4) === '.svg') {
-						comileDebounce();
-					}
-				});
-				return;
+			const svgs = [].concat(options.svgs);
+			const comileDebounce = thro_debs.debounce(800, build.bind(this));
+
+			// 只有一个文件夹时，监视文件夹。支持新增。/ab/c/*.svg
+			if (svgs.length === 1) {
+				const dir = path.dirname(svgs[0]).replace('*.svg', '');
+				if (fs.existsSync(dir) && fs.statSync(dir).isDirectory()) {
+					watchers1 = fs.watch(dir, (event, filename) => {
+						if (filename.length > 4 && filename.slice(-4) === '.svg') {
+							comileDebounce();
+						}
+					});
+					return;
+				}
 			}
-		}
 
-		// 监视每个文件。/ab/c/**/*.svg
-		globby(svgs).then(files => {
-			files.forEach(file => {
-				fs.watch(file, (event, filename) => {
-					comileDebounce();
+			// 监视每个文件。/ab/c/**/*.svg
+			globby(svgs).then(files => {
+				files.forEach(file => {
+					watchers2 = fs.watch(file, (event, filename) => {
+						comileDebounce();
+					});
 				});
 			});
-		});
+
+			process.on('SIGHUP', function () {
+				watchers1?.close();
+				watchers2?.close();
+				process.exit();
+			});
+		}
 	};
+
 	return {
 		name: 'rollupPluginIconfont',
 		buildStart() {
